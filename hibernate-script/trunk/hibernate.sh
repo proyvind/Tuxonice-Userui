5d84a33b1d61688f7a7ee2595741827bc5b34642
Bernard Blackham <bernard@tuxonice.net>
2004-07-16 10:44:35 +0000

    suspend -> hibernate

--- hibernate-script/trunk/hibernate.sh
+++ hibernate-script/trunk/hibernate.sh
@@ -4,7 +4,7 @@
 #
 # Suspend Script - Copyright (C) 2004 Bernard Blackham <bernard@blackham.com.au>
 # 
-# The suspend-script package is free software; you can redistribute it and/or
+# The hibernate-script package is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2, or (at your option) any later
 # version.
