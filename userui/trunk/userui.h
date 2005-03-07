89455430596f6fef2fa36e690d6a24563516dc9c
Bernard Blackham <bernard@tuxonice.net>
2005-03-07 03:42:44 +0000

    move generic_fls out into common code

--- userui/trunk/userui.h
+++ userui/trunk/userui.h
@@ -51,5 +51,39 @@ extern int suspend_action;
 #define SUSPEND_RETRY_RESUME		23
 #define SUSPEND_VGA_POST		24
 
+/* excerpts from include/linux/bitops.h */
+/*
+ * fls: find last bit set.
+ */
+
+static __inline__ int generic_fls(int x)
+{
+	int r = 32;
+
+	if (!x)
+		return 0;
+	if (!(x & 0xffff0000u)) {
+		x <<= 16;
+		r -= 16;
+	}
+	if (!(x & 0xff000000u)) {
+		x <<= 8;
+		r -= 8;
+	}
+	if (!(x & 0xf0000000u)) {
+		x <<= 4;
+		r -= 4;
+	}
+	if (!(x & 0xc0000000u)) {
+		x <<= 2;
+		r -= 2;
+	}
+	if (!(x & 0x80000000u)) {
+		x <<= 1;
+		r -= 1;
+	}
+	return r;
+}
+
 
 #endif /* _USERUI_H_ */
--- userui/trunk/userui_text.c
+++ userui/trunk/userui_text.c
@@ -11,41 +11,7 @@
 
 #include "userui.h"
 
-/* excerpts from include/linux/bitops.h */
-/*
- * fls: find last bit set.
- */
-
-static __inline__ int generic_fls(int x)
-{
-	int r = 32;
-
-	if (!x)
-		return 0;
-	if (!(x & 0xffff0000u)) {
-		x <<= 16;
-		r -= 16;
-	}
-	if (!(x & 0xff000000u)) {
-		x <<= 8;
-		r -= 8;
-	}
-	if (!(x & 0xf0000000u)) {
-		x <<= 4;
-		r -= 4;
-	}
-	if (!(x & 0xc0000000u)) {
-		x <<= 2;
-		r -= 2;
-	}
-	if (!(x & 0x80000000u)) {
-		x <<= 1;
-		r -= 1;
-	}
-	return r;
-}
-
-/* Now we essentially cut and paste the suspend_text plugin */
+/* We essentially cut and paste the suspend_text plugin */
 
 static int barwidth = 0, barposn = -1, newbarposn = 0;
 static int draw_progress_bar = 1;
