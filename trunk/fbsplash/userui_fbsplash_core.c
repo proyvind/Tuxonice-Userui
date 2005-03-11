c0a54ebeb00c1a9bada37014d3b361f10915c8fa
Bernard Blackham <bernard@tuxonice.net>
2005-03-11 16:58:40 +0000

    Experimental patch for getting colourmaps rights

--- trunk/fbsplash/userui_fbsplash_core.c
+++ trunk/fbsplash/userui_fbsplash_core.c
@@ -25,8 +25,14 @@ static int base_image_size;
 static inline void disable_utf8() { write(1, "\033%@", 3); }
 static inline void clear_display() { write(1, "\033[2J", 3); }
 static inline void move_cursor_to(int c, int r) { printf("\033[%d;%dH", r, c); }
-static void hide_cursor() { write(1, "\033[?25l\033[?1c", 9); }
-static void show_cursor() { write(1, "\033[?25h\033[?0c", 9); }
+static void hide_cursor() {
+	ioctl(STDOUT_FILENO, KDSETMODE, KD_GRAPHICS);
+ 	write(1, "\033[?25l\033[?1c", 9);
+}
+static void show_cursor() {
+	ioctl(STDOUT_FILENO, KDSETMODE, KD_TEXT);
+	write(1, "\033[?25h\033[?0c", 9);
+}
 
 static void reset_silent_img() {
 	if (!base_image || !silent_img.data)
