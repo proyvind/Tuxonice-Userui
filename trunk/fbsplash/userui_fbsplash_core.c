302f80cea006aabf9cf3add97cad8da55fcb39a0
Henrik Brix Andersen <brix@gentoo.org>
2005-03-11 13:02:48 +0000

    Allow aborting suspend process by hitting ESC

--- trunk/fbsplash/userui_fbsplash_core.c
+++ trunk/fbsplash/userui_fbsplash_core.c
@@ -217,6 +217,9 @@ static void fbsplash_log_level_change() {
 
 static void fbsplash_keypress(int key) {
 	switch (key) {
+		case 27:
+			send_message(USERUI_MSG_ABORT, NULL, 0);
+			break;
 		case 48:
 		case 49:
 		case 50:
