8ac735f20d2b1e103e5daedb1271fedbf4c09acf
Bernard Blackham <bernard@tuxonice.net>
2006-06-06 17:26:12 +0000

    Release 0.6.4

---
+++ tags/release-0.6.4/fbsplash/common.c
@@ -0,0 +1,290 @@
+/*
+ * common.c - miscellaneous functions used by both the kernel helper and
+ *            user utilities.
+ * 
+ * Suspend2 userui adaptations:
+ *   Copyright (C) 2005 Bernard Blackham <bernard@blackham.com.au>
+ *
+ * Based on the original splashutils code:
+ * Copyright (C) 2004-2005, Michal Januszewski <spock@gentoo.org>
+ * 
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License v2.  See the file COPYING in the main directory of this archive for
+ * more details.
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <sys/vt.h>
+#include <linux/kd.h>
+
+#include "splash.h"
+
+struct fb_var_screeninfo   fb_var;
+struct fb_fix_screeninfo   fb_fix;
+
+enum ENDIANESS endianess;
+char *config_file = NULL;
+
+enum TASK arg_task = none; 
+int arg_fb = 0;
+int arg_vc = 0;
+char arg_mode = 'v';
+char *arg_theme = NULL;
+u16 arg_progress = 0;
+char *progress_text = NULL;
+u8 arg_kdmode = KD_TEXT;
+
+#ifndef TARGET_KERNEL
+char *arg_export = NULL;
+#endif
+
+int bytespp = 4;		/* bytes per pixel */
+u8 fb_opt = 0;			/* can we use optimized 24/32bpp routines? */
+u8 fb_ro, fb_go, fb_bo; 	/* red, green, blue offset */
+u8 fb_rlen, fb_glen, fb_blen;	/* red, green, blue length */
+
+struct fb_image pic;
+char *pic_file = NULL;
+
+void detect_endianess(void)
+{
+	u16 t = 0x1122;
+
+	if (*(u8*)&t == 0x22) {
+		endianess = little;
+	} else {
+		endianess = big;
+	}
+
+	DEBUG("This system is %s-endian.\n", (endianess == little) ? "little" : "big");
+}
+
+int get_fb_settings(int fb_num)
+{
+	char fn[32];
+	int fb;
+#ifdef TARGET_KERNEL
+	char sys[128];
+#endif
+
+	sprintf(fn, PATH_DEV "/fb/%d", fb_num);	
+	fb = open(fn, O_WRONLY, 0);
+
+	if (fb == -1) {
+		sprintf(fn, PATH_DEV "/fb%d", fb_num);
+		fb = open(fn, O_WRONLY, 0);
+	}
+	
+	if (fb == -1) {
+#ifdef TARGET_KERNEL
+		sprintf(sys, PATH_SYS "/class/graphics/fb%d/dev", fb_num);
+		create_dev(fn, sys, 0x1);
+		fb = open(fn, O_WRONLY, 0);
+		if (fb == -1)
+			remove_dev(fn, 0x1);
+		if (fb == -1)
+#endif
+		{
+			printerr("Failed to open " PATH_DEV "/fb%d or " PATH_DEV 
+				 "/fb%d for reading.\n", fb_num, fb_num);
+			return 1;
+		}
+	}
+		
+	if (ioctl(fb,FBIOGET_VSCREENINFO,&fb_var) == -1) {
+		printerr("Failed to get fb_var info.\n");
+		return 2;
+	}
+
+	if (ioctl(fb,FBIOGET_FSCREENINFO,&fb_fix) == -1) {
+		printerr("Failed to get fb_fix info.\n");
+		return 3;
+	}
+
+	close(fb);
+
+#ifdef TARGET_KERNEL
+	remove_dev(fn, 0x1);
+#endif
+	bytespp = (fb_var.bits_per_pixel + 7) >> 3;
+
+	/* Check if optimized code can be used. We use special optimizations for
+	 * 24/32bpp modes in which all color components have a length of 8 bits. */
+	if (bytespp < 3 || fb_var.blue.length != 8 || fb_var.green.length != 8 || fb_var.red.length != 8) {
+		fb_opt = 0;
+
+		if (fb_fix.visual == FB_VISUAL_DIRECTCOLOR) {
+			fb_blen = fb_glen = fb_rlen = min(min(fb_var.red.length,fb_var.green.length),fb_var.blue.length);
+		} else {
+			fb_rlen = fb_var.red.length;
+			fb_glen = fb_var.green.length;
+			fb_blen = fb_var.blue.length;
+		}
+	} else {
+		fb_opt = 1;
+
+		/* Compute component offsets (ie. indexes in an array of u8's) */
+		fb_ro = (fb_var.red.offset >> 3);
+		fb_go = (fb_var.green.offset >> 3);
+		fb_bo = (fb_var.blue.offset >> 3);
+			
+		if (endianess == big) {
+			fb_ro = bytespp - 1 - fb_ro;
+			fb_go = bytespp - 1 - fb_go;
+			fb_bo = bytespp - 1 - fb_bo;
+		}
+	}
+
+	return 0;
+}
+
+char *get_filepath(char *path) 
+{
+	char buf[512];
+	
+	if (path[0] == '/')
+		return strdup(path);
+
+	snprintf(buf, 512, "%s/%s/%s", THEME_DIR, arg_theme, path);
+	return strdup(buf);	
+}
+
+char *get_cfg_file(char *theme) 
+{
+	char buf[512];
+
+	snprintf(buf, 512, "%s/%s/%dx%d.cfg", THEME_DIR, theme, fb_var.xres, fb_var.yres);
+	return strdup(buf);	
+}
+
+int do_getpic(unsigned char origin, unsigned char do_cmds, char mode)
+{	
+	if (load_images(mode))
+		return -1;
+
+#if (defined(CONFIG_TTF) && !defined(TARGET_KERNEL)) || (defined(CONFIG_TTF_KERNEL) && defined(TARGET_KERNEL))
+	load_fonts();
+#endif
+	
+#ifdef CONFIG_FBSPLASH
+	if (do_cmds) {
+		cmd_setpic(&verbose_img, origin);
+		free((u8*)verbose_img.data);
+		if (verbose_img.cmap.red);
+			free(verbose_img.cmap.red);
+	}	
+#endif
+	return 0;
+}
+
+#ifdef CONFIG_FBSPLASH
+int do_config(unsigned char origin)
+{
+	if (!config_file) {
+		printerr("No config file.\n");
+		return -1;
+	}
+		
+	/* If the user specified invalid values for the text field - correct it.
+	 * Also setup default values (text field coverting the whole screen). */
+	if (cf.tx > fb_var.xres)
+		cf.tx = 0;
+
+	if (cf.ty > fb_var.yres)
+		cf.ty = 0;
+
+	if (cf.tw > fb_var.xres || cf.tw == 0)
+		cf.tw = fb_var.xres;
+
+	if (cf.th > fb_var.yres || cf.th == 0)
+		cf.th = fb_var.yres;
+
+	cmd_setcfg(origin);
+	return 0;
+}
+#endif
+
+void vt_cursor_disable(int fd)
+{
+	write(fd, "\e[?25l\e[?1c",11);
+}
+
+void vt_cursor_enable(int fd)
+{
+	write(fd, "\e[?25h\e[?0c",11);
+}
+
+int open_fb()
+{
+	char dev[32];
+	int c;
+	
+	sprintf(dev, PATH_DEV "/fb%d", arg_fb);
+	if ((c = open(dev, O_RDWR)) == -1) {
+		sprintf(dev, PATH_DEV "/fb/%d", arg_fb);
+		if ((c = open(dev, O_RDWR)) == -1) {
+			printerr("Failed to open " PATH_DEV "/fb%d or " 
+			         PATH_DEV "/fb/%d.\n", arg_fb, arg_fb);
+			return -1;
+		}
+	}
+
+	return c;
+}
+
+int open_tty(int tty)
+{
+	char dev[32];
+	int c;
+
+	sprintf(dev, PATH_DEV "/tty%d", tty);
+	if ((c = open(dev, O_RDWR)) == -1) {
+		sprintf(dev, PATH_DEV "/vc/%d", tty);
+		if ((c = open(dev, O_RDWR)) == -1) {
+			printerr("Failed to open " PATH_DEV "/tty%d or " 
+				 PATH_DEV "/vc/%d\n", tty, tty);
+			return 0;
+		}
+	}
+
+	return c;
+}
+
+int tty_set_silent(int tty, int fd)
+{
+	struct termios w;
+	
+	tcgetattr(fd, &w);
+	w.c_lflag &= ~(ICANON|ECHO);
+	w.c_cc[VTIME] = 0;
+	w.c_cc[VMIN] = 1;
+	tcsetattr(fd, TCSANOW, &w);
+	vt_cursor_disable(fd);
+	ioctl(fd, VT_ACTIVATE, tty);
+	ioctl(fd, VT_WAITACTIVE, tty);
+
+	return 0;
+}
+
+int tty_unset_silent(int fd)
+{
+	struct termios w;
+	
+	tcgetattr(fd, &w);
+	w.c_lflag &= (ICANON|ECHO);
+	w.c_cc[VTIME] = 0;
+	w.c_cc[VMIN] = 1;
+	tcsetattr(fd, TCSANOW, &w);
+	vt_cursor_enable(fd);
+
+	return 0;
+}
---
+++ tags/release-0.6.4/userui.h
@@ -0,0 +1,131 @@
+#ifndef _USERUI_H_
+#define _USERUI_H_
+
+#include <linux/unistd.h>
+#include <sys/types.h>
+#include "suspend_userui.h"
+
+#define USERUI_VERSION "0.6.4"
+
+struct userui_ops {
+	char *name;
+	void (*prepare) ();
+	void (*cleanup) ();
+	void (*message) (unsigned long type, unsigned long level, int normally_logged, char *text);
+	void (*update_progress) (unsigned long value, unsigned long maximum, char *text);
+	void (*log_level_change) ();
+	void (*redraw) ();
+	void (*keypress) (int key);
+	unsigned long (*memory_required) ();
+	/* For extra cmdline options: */
+	char *optstring;
+	struct option *longopts;
+	int (*option_handler) (char c);
+	char *(*cmdline_options) ();
+};
+
+
+int send_message(int type, void* buf, int len);
+int common_keypress_handler(int key);
+void set_console_loglevel(int exiting);
+
+extern char software_suspend_version[32];
+extern int can_use_escape;
+extern volatile int console_loglevel;
+extern volatile int suspend_action;
+extern volatile int suspend_debug;
+extern volatile int resuming;
+
+/* excerpts from include/linux/suspend2.h : */
+
+/* debugging levels. */
+#define SUSPEND_STATUS		0
+#define SUSPEND_UI_MSG		1
+#define SUSPEND_ERROR		2
+#define SUSPEND_LOW	 	3
+#define SUSPEND_MEDIUM	 	4
+#define SUSPEND_HIGH	  	5
+#define SUSPEND_VERBOSE		6
+
+/* second status register */
+enum {
+	SUSPEND_REBOOT,
+	SUSPEND_PAUSE,
+	SUSPEND_SLOW,
+	SUSPEND_NOPAGESET2,
+	SUSPEND_LOGALL,
+	SUSPEND_CAN_CANCEL,
+	SUSPEND_KEEP_IMAGE,
+	SUSPEND_FREEZER_TEST,
+	SUSPEND_FREEZER_TEST_SHOWALL,
+	SUSPEND_SINGLESTEP,
+	SUSPEND_PAUSE_NEAR_PAGESET_END,
+	SUSPEND_USE_ACPI_S4,
+	SUSPEND_KEEP_METADATA,
+	SUSPEND_TEST_FILTER_SPEED,
+	SUSPEND_FREEZE_TIMERS,
+	SUSPEND_DISABLE_SYSDEV_SUPPORT,
+	SUSPEND_VGA_POST
+};
+
+/* Debug sections  - if debugging compiled in */
+enum {
+	SUSPEND_ANY_SECTION,
+	SUSPEND_FREEZER,
+	SUSPEND_EAT_MEMORY,
+	SUSPEND_PAGESETS,
+	SUSPEND_IO,
+	SUSPEND_BMAP,
+	SUSPEND_HEADER,
+	SUSPEND_WRITER,
+	SUSPEND_MEMORY,
+	SUSPEND_EXTENTS,
+	SUSPEND_SPINLOCKS,
+	SUSPEND_MEM_POOL,
+	SUSPEND_RANGE_PARANOIA,
+	SUSPEND_NOSAVE,
+	SUSPEND_INTEGRITY
+};
+
+/* excerpts from include/linux/bitops.h */
+/*
+ * fls: find last bit set.
+ */
+
+static __inline__ int generic_fls(int x)
+{
+	int r = 32;
+
+	if (!x)
+		return 0;
+	if (!(x & 0xffff0000u)) {
+		x <<= 16;
+		r -= 16;
+	}
+	if (!(x & 0xff000000u)) {
+		x <<= 8;
+		r -= 8;
+	}
+	if (!(x & 0xf0000000u)) {
+		x <<= 4;
+		r -= 4;
+	}
+	if (!(x & 0xc0000000u)) {
+		x <<= 2;
+		r -= 2;
+	}
+	if (!(x & 0x80000000u)) {
+		x <<= 1;
+		r -= 1;
+	}
+	return r;
+}
+
+/*
+ * Work around random glibc bugs where getpid() caches an invalid pid.
+ */
+#define __NR_xgetpid __NR_getpid
+static inline _syscall0(pid_t, xgetpid);
+
+
+#endif /* _USERUI_H_ */
