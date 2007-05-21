a25925e20101784d7f500a8bc440927d286866cb
Bernard Blackham <bernard@tuxonice.net>
2007-05-21 14:51:30 +0000

    Added --xres, --yres and --quiet options to usplash (Whoopie).

--- trunk/usplash/userui_usplash_core.c
+++ trunk/usplash/userui_usplash_core.c
@@ -13,6 +13,7 @@
 #include <sys/resource.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <string.h>
 #include <termios.h>
@@ -25,6 +26,9 @@
 #define MIN_ADVANCE_PERCENT 5
 
 static int usplash_ready = 0;
+static int userui_usplash_xres = 0;
+static int userui_usplash_yres = 0;
+static int userui_usplash_verbose = 1;
 
 static void userui_usplash_prepare() {
     /* Set RLIMIT_NPROC now to prevent svgalib from forking. */
@@ -39,7 +43,8 @@ static void userui_usplash_prepare() {
      */
     have_termios = (-1 != tcgetattr(STDIN_FILENO, &t));
 
-    if (usplash_setup(0, 0, 1)) {
+    if (usplash_setup(userui_usplash_xres, userui_usplash_yres,
+			userui_usplash_verbose)) {
 	usplash_restore_console();
 	if (have_termios)
 	    tcsetattr(STDIN_FILENO, TCSANOW, &t);
@@ -129,6 +134,42 @@ static void userui_usplash_keypress(int key) {
     }
 }
 
+static int usplash_option_handler(char c)
+{
+    switch (c) {
+	case 'x':
+	    sscanf(optarg, "%d", &userui_usplash_xres);
+	    break;
+	case 'y':
+	    sscanf(optarg, "%d", &userui_usplash_yres);
+	    break;
+	case 'q':
+	    userui_usplash_verbose = 0;
+	    break;
+	default:
+	    return 0;
+    }
+    return 1;
+}
+
+static char *usplash_cmdline_options()
+{
+    return 
+"  -x, --xres\n"
+"     Specifies the X resolution.\n"
+"  -y, --yres\n"
+"     Specifies the Y resolution.\n"
+"  -q, --quiet\n"
+"     Enables usplash's quiet mode (no text is shown).\n";
+}
+
+static struct option userui_usplash_longopts[] = {
+    {"xres", 1, 0, 'x'},
+    {"yres", 1, 0, 'y'},
+    {"quiet", 0, 0, 'q'},
+    {NULL, 0, 0, 0},
+};
+
 static struct userui_ops userui_usplash_ops = {
     .name = "usplash",
     .prepare = userui_usplash_prepare,
@@ -138,6 +179,12 @@ static struct userui_ops userui_usplash_ops = {
     .log_level_change = userui_usplash_log_level_change,
     .redraw = userui_usplash_redraw,
     .keypress = userui_usplash_keypress,
+
+    /* cmdline options */
+    .optstring = "x:y:q",
+    .longopts  = userui_usplash_longopts,
+    .option_handler = usplash_option_handler,
+    .cmdline_options = usplash_cmdline_options,
 };
 
 struct userui_ops *userui_ops = &userui_usplash_ops;
