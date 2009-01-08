2921c3a7e0ced396d317ba6e536cbfd3d7231541
Nigel Cunningham <nigel@tuxonice.net>
2009-01-08 01:55:07 +0000

    0.7.3 release.

---
+++ tags/release-0.7.3/fbsplash/list.c
@@ -0,0 +1,31 @@
+/*
+ * list.c - list utility functions 
+ * 
+ * TuxOnIce userui adaptations:
+ *   Copyright (C) 2005 Bernard Blackham <bernard@blackham.com.au>
+ *
+ * Based on the original splashutils code:
+ * Copyright (C) 2005 Michal Januszewski <spock@gentoo.org>
+ * 
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License v2.  See the file COPYING in the main directory of this archive for
+ * more details.
+ *
+ */
+#include <stdlib.h>
+#include "splash.h"
+
+void list_add(list *l, void *obj)
+{
+	if (l->tail != NULL) {
+		l->tail->next = malloc(sizeof(item));
+		l->tail->next->p = obj;
+		l->tail->next->next = NULL;
+		l->tail = l->tail->next;
+	} else {
+		l->head = l->tail = malloc(sizeof(item));
+		l->tail->next = NULL;
+		l->tail->p = obj;
+	}
+}
+
